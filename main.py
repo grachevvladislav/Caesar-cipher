015949dec3c30f7fdd295ba16752f746ae16e736
Vladislav0Grachev <95340858+Vladislav0Grachev@users.noreply.github.com>
2022-01-30 10:33:29 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,55 +1,66 @@
-alfavit =  " 1234567890абвгдеёжзийклмнопрстуфхцчшщъыьэюя.,;:!?'«»—()"
-message = ""
-f = open('book.txt', 'r')
-key = 7
-
-for i in f.read():
-    if i.lower() in alfavit:
-        message += i.lower()
-
 def encrypt_message(message, key):
     encrypted = ''
-    for i in message:
-        n = alfavit.find(i) + key
-        if n >= len(alfavit):
-            n -= len(alfavit)
-        if n < 0:
-            n += len(alfavit)
-        encrypted += alfavit[n]
+    for letter in message:
+        if ord(letter) > 96 and ord(letter) < 123:
+            pre_ord = ord(letter) + key
+            if pre_ord > 122:
+                pre_ord -= 26
+            if pre_ord < 97:
+                pre_ord += 26
+            encrypted += chr(pre_ord)
+        else:
+            encrypted += letter
     return encrypted
 
 
 def decrypt_message(message, key):
     decrypted = ''
-    for i in message:
-        n = alfavit.find(i) - key
-        if n >= len(alfavit):
-            n -= len(alfavit)
-        if n < 0:
-            n += len(alfavit)
-        decrypted += alfavit[n]
+    for letter in message:
+        if ord(letter) > 96 and ord(letter) < 123:
+            pre_ord = ord(letter) - key
+            if pre_ord > 122:
+                pre_ord -= 26
+            if pre_ord < 97:
+                pre_ord += 26
+            decrypted += chr(pre_ord)
+        else:
+            decrypted += letter
     return decrypted
 
-def hacking(message):
-    counter = dict.fromkeys(alfavit, 0)
-    for i in message:
-        if i in counter:
-            counter[i] += 1
-    maxcount = 0
-    maxletter = '0'
 
-    for letter in counter:
-        if counter[letter] > maxcount:
-            maxcount = counter[letter]
-            maxletter = letter
-    newkey = alfavit.find(maxletter)
-    return newkey
-print(message.lower())
-encrypted = encrypt_message(message.lower(), key)
+def hacking(message):
+    the_list = []
+    for letter in message:
+        if ord(letter) > 96 and ord(letter) < 123:
+             if the_list == []:
+                 the_list.append(ord(letter))
+             elif len(the_list) == 1 and (the_list[0] - ord(letter) == 12 or the_list[0] - ord(letter) == -14):
+                 the_list.append(ord(letter))
+             elif len(the_list) == 2 and (the_list[0] - ord(letter) == 15 or the_list[0] - ord(letter) == -11):
+                 the_list.append(ord(letter))
+             else:
+                 the_list = []
+        else:
+            if len(the_list) == 3:
+                key = the_list[0] - 116
+                if key <= 0: key+= 26
+                return key
+            the_list = []
+    return 0
+
+d = open('book.txt', 'r')
+text = d.read()
+key = 8
+
+print(text)
+
+encrypted = encrypt_message(text, key)
 print(encrypted)
 
 newkey = hacking(encrypted)
-print(newkey)
+print(newkey, newkey == key)
 
 decrypted = decrypt_message(encrypted, newkey)
 print(decrypted)
+
+d.close()
